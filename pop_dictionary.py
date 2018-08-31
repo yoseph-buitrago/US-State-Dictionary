6b702429c4b67d71301dc7f7313017345a28a73f
Yoseph Buitrago <yosephgbuitrago@hotmail.com>
2018-08-31 16:35:07 -0700

    put dictionary in own file
    separated dictionary from the source code and used an import statement

---
+++ pop_dictionary.py
@@ -0,0 +1,27 @@
+states = {
+    "California": [ 39000000, "Sacramento", "Los Angeles", "The Golden State" ],
+    "New York": [ 8000000, "Albany", "New York City", "The Empire State" ],
+    "Florida": [ 21000000, "Tallahassee", "Maiami", "The Sunshine State" ],
+    "Alaska": [ 739795, "Juneau", "Anchorage", "The Last Frontier" ],
+    "Hawaii": [ 1000000, "Honolulu", "Honolulu", "The Aloha State" ],
+    "Tennessee": [ 7000000, "Nashville", "Nashville", "The Volunteer State" ],
+    "Kentucky": [ 4000000, "Frankfort", "Louisville", "Bluegrass State" ],
+    "Louisiana": [ 5000000, "Baton Rouge", "New Orleans", "Child of the Mississippi, Creole State, Bayou State, Sugar State, Sportsman's Paradise, and Pelican State" ],
+    "Delaware": [ 961939, "Dover", "Wilmington", "The First State, The Diamond State, The Small Wonder, and Blue Hen State" ], 
+    "Wyoming": [ 579315, "Cheyenne", "Cheyenne", "Equality State, Cowboy State, and Big Wyoming" ],
+    "South Dakota": [ 869666, "Pierre", "Sioux Falls", "The Mount Rushmore State" ],
+    "West Virginia": [ 2000000, "Charleston", "Charleston", "Mountain State" ],
+    "Maryland": [ 6000000, "Annapolis", "Baltimore", "Old Line State, Free State, and Little America" ],
+    "Wisconsin": [ 6000000, "Madison", "Milwaukee", "America's Dairyland and Badger State" ],
+    "New Mexico": [ 2000000, "Santa Fe", "Albuquerque", "Land of Enchantment" ],
+    "Maine": [ 1000000, "Augusta", "Portland", "The Pine Tree State" ], 
+    "Iowa": [ 3000000, "Des Moines", "Des Moines", "The Hawkeye State" ],
+    "Ohio": [ 12000000, "Colombus", "Colombus", "The Mother of Presidents, The Heart of it All, The Buckeye State, and Birthplace of Aviation" ],
+    "New Jersey": [ 9000000, "Trenton", "Newark", "The Garden State" ],
+    "Washington": [ 7000000,"Olympia", "Seattle", "The Evergreen State" ],
+    "Georgia": [ 1000000, "Atlanta", "Atlanta", "Peach State and Empire State of the South" ],
+    "Missouri": [ 6000000, "Jefferson City", "Saint Louis", "The Show-Me State" ],
+    "Mississippi": [ 3000000, "Jackson", "Jackson", "The Magnolia State and the Hospitality State" ]
+
+    }
+
